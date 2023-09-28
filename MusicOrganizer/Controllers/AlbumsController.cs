2279d220b0962b4244f17fad8966c59b81f9dfdd
Jeremy Josol <jeremyjosol@gmail.com>
2023-09-28 13:09:05 -0700

    Focus on creative exploration

--- MusicOrganizer/Controllers/AlbumsController.cs
+++ MusicOrganizer/Controllers/AlbumsController.cs
@@ -13,5 +13,38 @@ namespace MusicOrganizer.Controllers
       List<Album> allAlbums = Album.GetAll();
       return View(allAlbums);
     }
+    [HttpGet("/albums/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+    [HttpPost("/albums")]
+    public ActionResult Create(string albumName)
+    {
+      Album newAlbum = new Album(albumName);
+      return RedirectToAction("Index");
+    }
+    [HttpGet("/albums/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Album selectedAlbum = Album.Find(id);
+      List<Song> albumSongs = selectedAlbum.Songs;
+      model.Add("album", selectedAlbum);
+      model.Add("songs", albumSongs);
+      return View(model);
+    }
+    [HttpPost("/albums/{albumId}/songs")]
+    public ActionResult Create(int albumId, string songTitle)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Album foundAlbum = Album.Find(albumId);
+      Song newSong = new Song(songTitle);
+      foundAlbum.AddSong(newSong);
+      List<Song> albumSongs = foundAlbum.Songs;
+      model.Add("songs", albumSongs);
+      model.Add("album", foundAlbum);
+      return View("Show", model);
+    }
   }
 }
---
+++ MusicOrganizer/Controllers/SongsController.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using MusicOrganizer.Models;
+
+namespace MusicOrganizer.Controllers
+{
+  public class SongsController : Controller
+  {
+    [HttpGet("albums/{albumId}/songs/new")]
+    public ActionResult New(int albumId)
+    {
+      Album album = Album.Find(albumId);
+      return View(album);
+    }
+    [HttpGet("/albums/{albumId}/songs/{songId}")]
+    public ActionResult Show(int albumId, int songId)
+    {
+      Song song = Song.Find(songId);
+      Album album = Album.Find(albumId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("song", song);
+      model.Add("album", album);
+      return View(model);
+    }
+  }
+}
