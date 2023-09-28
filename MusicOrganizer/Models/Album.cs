00ef483a5cd990684f2c975784c8c45d243986a0
Jeremy Josol <jeremyjosol@gmail.com>
2023-09-28 12:12:52 -0700

    Add Song child object in Album business logic

--- MusicOrganizer/Models/Album.cs
+++ MusicOrganizer/Models/Album.cs
@@ -8,12 +8,14 @@ namespace MusicOrganizer.Models
     private static List<Album> _instances = new List<Album> { };
     public string Name { get; set; }
     public int Id { get; }
+    public List<Song> Songs { get; set; }
   
     public Album(string albumName)
     {
       Name = albumName;
       _instances.Add(this);
       Id = _instances.Count;
+      Songs = new List<Song>{};
     }
     public static void ClearAll()
     {
@@ -26,6 +28,10 @@ namespace MusicOrganizer.Models
     public static Album Find(int searchId)
     {
       return _instances[searchId-1];
+    }
+    public void AddSong(Song song)
+    {
+      Songs.Add(song);
     }
   }
 }
