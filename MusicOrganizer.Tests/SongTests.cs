57fa7f4d98ef4116ca101b733af2e1959ca9ba0c
Jeremy Josol <jeremyjosol@gmail.com>
2023-09-28 12:07:39 -0700

    Create new test + pass test

--- MusicOrganizer.Tests/SongTests.cs
+++ MusicOrganizer.Tests/SongTests.cs
@@ -20,5 +20,16 @@ namespace MusicOrganizer.Tests
       string result = newSong.Title;
       Assert.AreEqual(songTitle, result);
     }
+    [TestMethod]
+    public void GetAll_ReturnsSongs_SongList()
+    {
+      string songTitle01 = "Forever";
+      string songTitle02 = "Country";
+      Song newSong1 = new Song(songTitle01);
+      Song newSong2 = new Song(songTitle02);
+      List<Song> newList = new List<Song> { newSong1, newSong2 };
+      List<Song> result = Song.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
   }
 }
