6b98939f5cdb6e8a34bdd104000aee363ac26ac7
Jeremy Josol <jeremyjosol@gmail.com>
2023-10-11 22:57:06 -0700

    Comment out code

---
+++ SpotifyClone/Models/SpotifyCloneContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace SpotifyClone.Models
+{
+  public class SpotifyCloneContext : DbContext
+  {
+    public DbSet<Album> Albums { get; set; }
+    public DbSet<Song> Songs { get; set; }
+    public DbSet<Playlist> Playlists { get; set; }
+    
+    public ToDoListContext(DbContextOptions options) : base(options) { }
+  }
+}
