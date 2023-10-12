20e729fe44a9e2f9fb2c8d58e946fa438b545a5b
Jeremy Josol <jeremyjosol@gmail.com>
2023-10-11 22:24:10 -0700

    Refactor Album class

--- SpotifyClone/Models/Album.cs
+++ SpotifyClone/Models/Album.cs
@@ -5,44 +5,11 @@ namespace SpotifyClone.Models
 {
   public class Album
   {
-    private static List<Album> _instances = new List<Album> { };
-    public string Name { get; set; }
-    public int Id { get; }
-    public List<Song> Songs { get; set; }
+    public int AlbumId { get; set; }
+    public string Title { get; set; }
     public string Artist { get; set; }
-  
-    public Album(string albumName)
-    {
-      Name = albumName;
-      Artist = null;
-      _instances.Add(this);
-      Id = _instances.Count;
-      Songs = new List<Song>{};
-    }
-    // come back to this overload concept
-    public Album(string albumName, string artistName)
-    {
-      Name = albumName;
-      Artist = artistName;
-      _instances.Add(this);
-      Id = _instances.Count;
-      Songs = new List<Song>{};
-    }
-    public static void ClearAll()
-    {
-      _instances.Clear();
-    }
-    public static List<Album> GetAll()
-    {
-      return _instances;
-    }
-    public static Album Find(int searchId)
-    {
-      return _instances[searchId-1];
-    }
-    public void AddSong(Song song)
-    {
-      Songs.Add(song);
-    }
+    public string ReleaseDate { get; set; }
+    public string AlbumArtworkUrl { get; set; }
+    public List<Song> Songs { get; set; }
   }
 }
