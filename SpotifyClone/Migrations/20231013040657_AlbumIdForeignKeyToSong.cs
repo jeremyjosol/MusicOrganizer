ff6937f1ff9af1642b122b3b82311541fb6bee4d
Jeremy Josol <jeremyjosol@gmail.com>
2023-10-12 21:07:39 -0700

    Add foreign key AlbumId to Song

---
+++ SpotifyClone/Migrations/20231013040657_AlbumIdForeignKeyToSong.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SpotifyClone.Migrations
+{
+    public partial class AlbumIdForeignKeyToSong : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
